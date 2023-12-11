ec6aee15674dfaef5a85906bc0c090433680dc34
Ivan <67190437+Ivanm2114@users.noreply.github.com>
2023-12-11 22:26:29 +0300

    left only tuple

--- Message.h
+++ Message.h
@@ -10,6 +10,8 @@
 #include "Robot.h"
 #include <string>
 #include "boost/tuple/tuple.hpp"
+#include <iostream>
+
 using namespace std;
 
 
@@ -18,33 +20,103 @@ class BaseMessage {
 public:
     BaseMessage();
     BaseMessage(V data);
-    void getMessage() const;
-    virtual void getClass() const;
+    void setData(V data);
+    V getMessage() const;
+    virtual string getClass() const;
 protected:
     V data;
 };
 
-template<class V, class K>
+template<class K, class V>
 class EditRobotMessage: public BaseMessage<V> {
 public:
     EditRobotMessage();
-    EditRobotMessage(V data, K key);
-    bool checkData(V data);
+    EditRobotMessage(K key, V data);
     void setData(V data);
-    void getClass() const override;
-    tuple<V,K> getMessage();
+    void setKey(K key);
+    string getClass() const override;
+    tuple<K,V> getMessage() const;
 
 private:
     V data;
     K key;
 };
 
-template<class V>
+template<class K, class V>
 class EditConnectionMessage: public BaseMessage<V> {
 public:
     EditConnectionMessage();
-    EditConnectionMessage(Robot first, Robot Second, string mode);
-    void setRobots();
-    void getClass() const override;
-
+    EditConnectionMessage(Robot<K,V>& first, Robot<K,V>& second, string mode);
+    EditConnectionMessage(Robot<K,V>& first, Robot<K,V>& second, bool mode);
+    void setRobots(Robot<V,K>& first, Robot<V,K>& Second);
+    void setMode(string mode);
+    void setMode(bool mode);
+    string getClass() const override;
+private:
+    Robot<K,V>& first,second;
+    bool mode;
 };
+
+template<class V>
+BaseMessage<V>::BaseMessage() {};
+
+template<class V>
+BaseMessage<V>::BaseMessage(V data) {
+    this->data = data;
+};
+
+template<class V>
+V BaseMessage<V>::getMessage() const {
+    return data;
+};
+
+
+template<class V>
+void BaseMessage<V>::setData(V data) {
+
+}
+
+template<class V>
+string BaseMessage<V>::getClass() const {
+    return "BaseMessage";
+};
+
+
+template<class K,class V>
+EditRobotMessage<K,V>::EditRobotMessage() {};
+
+template<class K,class V>
+EditRobotMessage<K,V>::EditRobotMessage(K key,V data) {
+    this->key = key;
+    this->data = data;
+};
+
+
+template<class K,class V>
+void EditRobotMessage<K,V>::setData(V data){
+    if(bool(data)){
+        this->data=data;
+    }
+    else{
+        cout << "This data can`t be set";
+    }
+};
+
+template<class K,class V>
+void EditRobotMessage<K,V>::setKey(K key){
+    if(bool(key)){
+        this->key=key;
+    }
+    else{
+        cout << "This key can`t be set";
+    }
+};
+
+
+template<class K,class V>
+string EditRobotMessage<K,V>::getClass() const{
+    return "EditRobotMessage";
+}
+
+template<class K,class V>
+tuple<K,V> EditRobotMessage<K,V>::getMessage() const{}
--- Robot.h
+++ Robot.h
@@ -7,8 +7,29 @@
 
 #endif //OOPPROJECT_ROBOT_H
 #include "Message.h"
+#include <unordered_map>
+#include <vector>
+#include <memory>
 
+using namespace std;
+
+template<class K,class V>
 class Robot{
 public:
     Robot();
+    Robot(string name);
+    Robot(string name, unordered_map<K,V> parameters);
+    void setParameters(unordered_map<K,V> parameters);
+    void setParameter(K key, V data);
+    void transferMessage(Robot& robot,BaseMessage<V> message) const;
+    void transferMessage(Robot& robot, EditRobotMessage<K,V> message) const;
+    void transferMessage(Robot& robot, EditConnectionMessage<K,V> message) const;
+    void applyChanges(EditRobotMessage<V,K> message);
+    void applyChanges(EditConnectionMessage<V,K> message);
+    void printStructure() const;
+
+private:
+    string name;
+    unordered_map<K,V> parameters;
+    vector<shared_ptr<Robot<K,V>>> connectedRobots;
 };
